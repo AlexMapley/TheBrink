5ee6d1a3f793b7ce71ac4335541dc286ef2b887a
AlexMapley <alexmapley@gmail.com>
2020-04-11 19:27:41 -0400

    stat characterPlayer creation

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ import (
 	"os"
 )
 
+// var player PlayerCharacter
+
 func main() {
-	fmt.Println("What is your plan?")
+	fmt.Println("What is your name?")
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
+	fmt.Print("Enter name: ")
+	name, _ := reader.ReadString('\n')
 
-	fmt.Printf("Your answer: %s\nGame Over", text)
+	player := PlayerCharacter{}
+	player.Character.Name = name
+	player.Character.health = 10
+
+	fmt.Printf("Your answer: %s\nGame Over", player.Character.Name)
 }
