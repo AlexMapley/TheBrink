3477d898447cb7099a64eeb0d59e5275d36f74a4
AlexMapley <alexmapley@gmail.com>
2020-04-11 19:22:04 -0400

    add initial user option

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Game Over")
+	fmt.Println("What is your plan?")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+
+	fmt.Printf("Your answer: %s\nGame Over", text)
 }
