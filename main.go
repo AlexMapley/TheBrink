4989b93defc0207d3e588f2d7beb3c2bd8728a2d
AlexMapley <alexmapley@gmail.com>
2020-04-11 20:07:34 -0400

    wrap first battle sequence in loop

--- main.go
+++ main.go
@@ -21,9 +21,11 @@ func main() {
 	bandit := NewBandit("Mel")
 	bandit.Character.Stats()
 
-	fmt.Printf("\n\nBandit Attacks %s\n\n\n", player.Character.Name)
-	bandit.Character.Attack(&player.Character)
-	player.Character.Stats()
+	for i := 0; i < 10; i++ {
+		fmt.Printf("\n\nBandit Attacks %s\n\n\n", player.Character.Name)
+		bandit.Character.Attack(&player.Character)
+		player.Character.Stats()
+	}
 
 	fmt.Printf("\n\nGame Over, %s\n\n\n", player.Character.Name)
 }
