abdc61468897cffd4bc7bf57e67e129a3b48c901
AlexMapley <alexmapley@gmail.com>
2020-04-11 20:01:05 -0400

    add bandit enemy type

---
+++ bandit.go
@@ -0,0 +1,21 @@
+package main
+
+type Bandit struct {
+	Character Character
+}
+
+func newPlayer(name string) Bandit {
+	bandit := Bandit{}
+
+	bandit.Character.Name = name
+
+	bandit.Character.Vitality = 3
+	bandit.Character.Strength = 4
+	bandit.Character.Agility = 6
+	bandit.Character.Intelligence = 5
+
+	bandit.Character.Health = (bandit.Character.Vitality * 8) + (bandit.Character.Strength * 2)
+	bandit.Character.Mana = (bandit.Character.Intelligence * 10)
+
+	return bandit
+}
