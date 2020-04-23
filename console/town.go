6e4cd2f424dcd7a3394326bd3cf16fa945645bc5
AlexMapley <alexmapley@gmail.com>
2020-04-22 20:55:02 -0400

    add patrol town option

--- console/town.go
+++ console/town.go
@@ -6,6 +6,8 @@ func NewTownConsole() Console {
 	// set default options
 	actions := make(map[int]string)
 	actions[1] = "End the day"
+	actions[2] = "Patrol the town"
+
 	console.Actions = actions
 
 	return console
