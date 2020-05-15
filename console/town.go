82286befb1f11f8300ef7f9942af1859dc3e9e32
AlexMapley <alexmapley@gmail.com>
2020-05-14 20:49:05 -0400

    fix runtime error

--- console/town.go
+++ console/town.go
@@ -4,7 +4,7 @@ func NewTownConsole() Console {
 	console := Console{}
 
 	// set default options
-	actions := make([]string, 3)
+	actions := make([]string, 4)
 	actions[0] = "End the day"
 	actions[1] = "Patrol the town"
 	actions[2] = "Stats"
