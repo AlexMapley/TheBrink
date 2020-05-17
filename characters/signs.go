1fdde71ab30a238fbf6cc163c9a34296c2ad7e0c
AlexMapley <alexmapley@gmail.com>
2020-05-17 19:42:06 -0400

    reorder signs, weaken wizard base stats

--- characters/signs.go
+++ characters/signs.go
@@ -1,38 +1,5 @@
 package characters
 
-func Warrior(character Character) Character{
-	res := character
-
-	res.Stats.Class = "Warrior"
-
-	// Raw Stat Boosts
-	res.Stats.Vitality += 4
-	res.Stats.Strength += 4
-	res.Stats.Agility += 2
-	res.Stats.Intelligence += 1
-	res.Stats.Expertise += 1
-	res.Stats.Block += 1
-
-	// Levelling Stat Boosts
-	res.Stats.LevelBonuses.Vitality += 1
-	res.Stats.LevelBonuses.Strength += 1
-	res.Stats.LevelBonuses.Expertise += 1
-	res.Stats.LevelBonuses.Block += 1
-
-	// Add Skills
-	res.SkillSlots = append(
-		res.SkillSlots, 
-		Skill{
-			Name: "DoubleStrike",
-			Cost: 35,
-			CoolDownMax: 4,
-			CoolDown: 0,
-		},
-	)
-
-	return res
-}
-
 func Rogue(character Character) Character{
 	res := character
 
@@ -83,6 +50,39 @@ func Vagabond(character Character) Character{
 	return res
 }
 
+func Warrior(character Character) Character{
+	res := character
+
+	res.Stats.Class = "Warrior"
+
+	// Raw Stat Boosts
+	res.Stats.Vitality += 4
+	res.Stats.Strength += 4
+	res.Stats.Agility += 2
+	res.Stats.Intelligence += 1
+	res.Stats.Expertise += 1
+	res.Stats.Block += 1
+
+	// Levelling Stat Boosts
+	res.Stats.LevelBonuses.Vitality += 1
+	res.Stats.LevelBonuses.Strength += 1
+	res.Stats.LevelBonuses.Expertise += 1
+	res.Stats.LevelBonuses.Block += 1
+
+	// Add Skills
+	res.SkillSlots = append(
+		res.SkillSlots, 
+		Skill{
+			Name: "DoubleStrike",
+			Cost: 35,
+			CoolDownMax: 4,
+			CoolDown: 0,
+		},
+	)
+
+	return res
+}
+
 func Wizard(character Character) Character{
 	res := character
 
@@ -91,10 +91,10 @@ func Wizard(character Character) Character{
 	// Raw Stat Boosts
 	res.Stats.Vitality += 2
 	res.Stats.Strength += 2
-	res.Stats.Agility += 3
+	res.Stats.Agility += 2
 	res.Stats.Intelligence += 8
-	res.Stats.Expertise += 2
-	res.Stats.Block += 2
+	res.Stats.Expertise += 1
+	res.Stats.Block += 1
 
 	// Levelling Stat Boosts
 	res.Stats.LevelBonuses.Intelligence += 2
