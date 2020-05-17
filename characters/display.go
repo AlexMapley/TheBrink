9007440d18d7edfe8695994e12a1a24033bfdc23
AlexMapley <alexmapley@gmail.com>
2020-05-17 15:51:11 -0400

    improve stats display and fix errors

--- characters/display.go
+++ characters/display.go
@@ -5,19 +5,20 @@ import (
 )
 
 func (stats *Stats) Display() {
-	color.Cyan("\n-------------\n%s\n-------------\nLevel: %d\nClass: %s\nHealth: %d/%d\nFocus: %d/%d\nVitality: %d\nStrength: %d\nAgility: %d\nIntelligence: %d\nCritical: %d\nDodge: %d\n\n",
+	color.Cyan("\n-------------\n%s\n-------------\nLevel: %d\nClass: %s\nHealth: %d/%d\nFocus: %d/%d\nVitality: %d\nStrength: %d\nAgility: %d\nIntelligence: %d\nCritical: %d\nDodge: %d\nAccuracy Rating: %d\n\n",
 		stats.Name,
 		stats.Level,
 		stats.Class,
 		stats.Health,
-		stats.MaxHealth,
-		stats.MaxFocus,
+		stats.GetMaxHealth(),
+		stats.GetMaxFocus(),
 		stats.Focus,
 		stats.Vitality,
 		stats.Strength,
 		stats.Agility,
 		stats.Intelligence,
-		stats.CriticalValue(),
-		stats.DodgeValue(),
+		stats.GetCriticalValue(),
+		stats.GetDodgeValue(),
+		stats.GetAccuracyRating(),
 	)
 }
