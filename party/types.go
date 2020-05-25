8e8d7b264d9f1c1fd67914554f4a2318ee20fac8
AlexMapley <alexmapley@gmail.com>
2020-05-25 16:15:42 -0400

    fix 2nd cyclic import bug

--- party/types.go
+++ party/types.go
@@ -3,13 +3,11 @@ package party
 
 import (
 	"the_brink/characters"
-	"the_brink/world"
 )
 
 type Party struct {
 	XCoordinate int
 	YCoordinate int
-	Tile world.Tile
 	Characters []characters.Character
 	Rune rune
 }
