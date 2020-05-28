14d5a61ada7b368795e917629c1f5e6eef1f8fe3
AlexMapley <alexmapley@gmail.com>
2020-05-28 19:05:11 -0400

    add back xp

--- characters/display.go
+++ characters/display.go
@@ -7,9 +7,10 @@ import (
 )
 
 func (stats *Stats) Display() {
-	text := fmt.Sprintf("\n-------------\n%s\n-------------\nLevel: %d\nClass: %s\nClassHash: %d\nHealth: %d/%d\nFocus: %d/%d\nVitality: %d\nStrength: %d\nAgility: %d\nIntelligence: %d\nCritical: %d\nDodge: %d\nBlock: %d\nAccuracy Rating: %d\n\n",
+	text := fmt.Sprintf("\n-------------\n%s\n-------------\nLevel: %d\nXP: %d\nClass: %s\nClassHash: %d\nHealth: %d/%d\nFocus: %d/%d\nVitality: %d\nStrength: %d\nAgility: %d\nIntelligence: %d\nCritical: %d\nDodge: %d\nBlock: %d\nAccuracy Rating: %d\n\n",
 		stats.Name,
 		stats.Level,
+		stats.XP,
 		stats.Class,
 		stats.ClassHash,
 		stats.Health,
