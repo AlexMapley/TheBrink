843367bee5d0430b3eaf1f17a41262dc005ed392
AlexMapley <alexmapley@gmail.com>
2020-12-18 17:30:10 -0500

    finish targetMember

--- party/batte.go
+++ party/batte.go
@@ -1,1 +1,19 @@
 package party
+
+import (
+	"math/rand"
+	"time"
+  )
+
+// TargetMember returns an attackble player from the party
+func (party *Party) TargetMember() *Character {
+
+	rand.Seed(time.Now().Unix())
+
+	for {
+		randomPartyMember := Party.Members[rand.Intn(len(Party.Members))]
+		if randomPartyMember.Health > 0 {
+			return randomPartyMember
+		}
+	}
+}
