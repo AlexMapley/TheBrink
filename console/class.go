158547830189b415ffdebf5440ce211334a429ea
AlexMapley <alexmapley@gmail.com>
2021-11-15 19:18:02 -0500

    improve class console displays

--- console/class.go
+++ console/class.go
@@ -41,7 +41,7 @@ func DisplayClassConsole(character *characters.Character) {
 
 	console := NewClassConsole(character)
 
-menuLoop:
+	menuLoop:
 	for {
 		option := console.ChooseAction(nil, nil)
 		if option == -1 {
